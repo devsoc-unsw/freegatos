8d231693a0a8a6abf97e31cb4fbdd0250f5df89f
Dicko Evaldo <dicko.evaldo@gmail.com>
2026-03-24 18:10:49 +1100

    Initial Commit

---
+++ freegato/freegatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  freegatoApp.swift
+//  freegato
+//
+//  Created by Dicko Evaldo on 24/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct freegatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
